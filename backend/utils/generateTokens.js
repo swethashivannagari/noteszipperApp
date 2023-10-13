44f3f40ca5ce8507a5d9095f89bf1519ff66c507
Swetha <mistii3004@gmail.com>
2023-10-13 22:32:26 +0530

    first version

--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -1,7 +1,8 @@
 const jwt=require('jsonwebtoken')
+require('dotenv').config()
 
 const generateToken=(id)=>{
-    const secretKey = 'swetha123'; 
+    const secretKey = process.env.JWT_SECRET; 
     return jwt.sign({id},secretKey,{
         expiresIn:"30d",
     })
